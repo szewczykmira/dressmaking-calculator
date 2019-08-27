6090a7972ecadb7da747d9057ffe9f1ac0251b5a
Mira <szewczyk.mira@gmail.com>
2019-08-27 16:13:49 +0200

    Add calculations

--- dressmaking/src/components/Calculator.js
+++ dressmaking/src/components/Calculator.js
@@ -4,18 +4,96 @@ import InputTable from "./InputTable"
 import OutputTable from "./OutputTable"
 
 export default class Calculator extends React.Component {
+  state = {
+      calculated: false,
+  }
   updateState = (name, value) => {
     this.setState({ [name]: value })
   }
+
+  calculateValues = () => {
+    this.setState({ calculated: true })
+    let {
+      opx,
+      opp,
+      ot,
+      ob,
+      ZWo,
+      ZTv,
+      ZKo,
+      SvRv,
+      dk,
+      luz1,
+      luz2,
+      luz3,
+      luz4,
+      luz5,
+    } = this.state
+    let a = opx - (8 / 7) * opp
+    let SyP = (1 / 9) * opp + 10.8 + dk
+    let PcPl = (1 / 7) * opp - 1.5 + luz2
+    let S3R2 = SvRv + luz1
+    let SyT = 0.25 * ZWo - 1
+    let SyS2 = (1 / 18) * opp + 2.1
+    let b = 0.25 * ob + 0.5 * luz4
+    let wb = 0.2 * (ob - ot) - 1.5
+    let z = 2 * (b - wb) - 0.5 * ot - luz5
+    let R3R4 = (1 / 18) * opp - (1 / 20) * a
+
+    this.setState({
+      SyP: SyP,
+      SyŁ: 0.5 * SyP,
+      SyT: SyT,
+      TB: 0.1 * ZWo + 4,
+      TK: ZTv - ZKo,
+      TyT: 2,
+      ByB: 2,
+      KyK: 2,
+      a: a,
+      PyPc: (1 / 7) * opp - 5.5 + luz1,
+      PcPl: PcPl,
+      PlPx: (2 / 7) * opp - 4 + 0.5 * a,
+      PyPx: (4 / 7) * opp + 0.5 * a + luz1 + luz2 + luz3,
+      PcPv: (2 / 3) * PcPl,
+      PxPp: (1 / 9) * opp + 0.8 + (1 / 8) * a,
+      SyS2: SyS2,
+      S2S3: 2,
+      S1R1: 1.2,
+      S3R2: S3R2,
+      //PlR3: PcR1 - 2 - 0.1*a,
+      R3R4: R3R4,
+      R4S7: S3R2 - 1,
+      TxTa: 0.25 * a,
+      TaS4: SyT + (1 / 18) * opp + (3 / 7) * a,
+      S4S6: SyS2,
+      S4Sx: SyS2 + 1.5,
+      S5X: (2 / 7) * opp + 0.25 * a + 4,
+      b: b,
+      wb: wb,
+      t: b - wb,
+      z: z,
+      zp: 0.4 * z,
+      zt: 0.6 * z,
+    })
+  }
+
   render = () => {
     return (
       <div className="container">
         <div className="row">
-          <div className="col s6">
+          <div className="col s12 m6">
             <InputTable passState={this.updateState} />
           </div>
-          <div className="col s6">
-            <OutputTable />
+          <div className="col s12 m6">
+            <button
+              className="waves-effect waves-light btn"
+              onClick={this.calculateValues}
+            >
+              Calculate!
+            </button>
+            <OutputTable
+              currentState={this.state.calculated ? this.state : null}
+            />
           </div>
         </div>
       </div>
--- dressmaking/src/components/OutputTable.js
+++ dressmaking/src/components/OutputTable.js
@@ -1,7 +1,17 @@
 import React from "react"
 
+let OutputRow = ({shortcut, values}) => {
+    return (
+        <tr>
+            <td>{shortcut}</td>
+            <td>{values ? values[shortcut] : null}</td>
+        </tr>
+    )
+}
+
 export default class OutputTable extends React.Component {
   render = () => {
+      let { currentState } = this.props
     return (
       <table className="striped responsive-table">
         <thead>
@@ -11,10 +21,39 @@ export default class OutputTable extends React.Component {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>SyK</td>
-            <td></td>
-          </tr>
+          <OutputRow shortcut="SyP" values={currentState} />
+          <OutputRow shortcut="SyŁ" values={currentState} />
+          <OutputRow shortcut="SyT" values={currentState} />
+          <OutputRow shortcut="TB" values={currentState} />
+          <OutputRow shortcut="TK" values={currentState} />
+          <OutputRow shortcut="TyT" values={currentState} />
+          <OutputRow shortcut="ByB" values={currentState} />
+          <OutputRow shortcut="KyK" values={currentState} />
+          <OutputRow shortcut="a" values={currentState} />
+          <OutputRow shortcut="PyPc" values={currentState} />
+          <OutputRow shortcut="PcPl" values={currentState} />
+          <OutputRow shortcut="PlPx" values={currentState} />
+          <OutputRow shortcut="PyPx" values={currentState} />
+          <OutputRow shortcut="PcPv" values={currentState} />
+          <OutputRow shortcut="PxPp" values={currentState} />
+          <OutputRow shortcut="SyS2" values={currentState} />
+          <OutputRow shortcut="S2S3" values={currentState} />
+          <OutputRow shortcut="S1R1" values={currentState} />
+          <OutputRow shortcut="S3R2" values={currentState} />
+          <OutputRow shortcut="PlR3" values={currentState} />
+          <OutputRow shortcut="R3R4" values={currentState} />
+          <OutputRow shortcut="P4S7" values={currentState} />
+          <OutputRow shortcut="TxTa" values={currentState} />
+          <OutputRow shortcut="TaS4" values={currentState} />
+          <OutputRow shortcut="S4S6" values={currentState} />
+          <OutputRow shortcut="S4Sx" values={currentState} />
+          <OutputRow shortcut="S5X" values={currentState} />
+          <OutputRow shortcut="b" values={currentState} />
+          <OutputRow shortcut="wb" values={currentState} />
+          <OutputRow shortcut="t" values={currentState} />
+          <OutputRow shortcut="z" values={currentState} />
+          <OutputRow shortcut="zp" values={currentState} />
+          <OutputRow shortcut="zt" values={currentState} />
         </tbody>
       </table>
     )
