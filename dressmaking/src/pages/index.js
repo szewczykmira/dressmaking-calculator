385d6aeb1bcc24318ef38be2f1d1536e30576eb8
Mira <szewczyk.mira@gmail.com>
2019-09-29 18:39:57 +0200

    Avoid window declaration during page building

--- dressmaking/src/pages/index.js
+++ dressmaking/src/pages/index.js
@@ -1,6 +1,8 @@
 import React from "react"
-import "materialize-css/dist/js/materialize.js"
-import "materialize-css/dist/css/materialize.css"
+if (typeof(window) !== `undefined` ) {
+  require("materialize-css/dist/js/materialize.js")
+  require("materialize-css/dist/css/materialize.css")
+}
 
 import Navbar from "../components/Navbar"
 import Calculator from "../components/Calculator"
